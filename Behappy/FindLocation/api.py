fbc2904b368f6332fba07b2ab6a4e8de4c06e85f
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-09-26 13:59:29 +0900

    Fix  : "문자열로 XML 데이터 받기"

--- Behappy/FindLocation/api.py
+++ Behappy/FindLocation/api.py
@@ -1,19 +1,41 @@
 from urllib.parse import urlencode, unquote, quote_plus
+import urllib
+import urllib.request
 import requests
 from bs4 import BeautifulSoup
+import xml.etree.ElementTree
+
+from requests.sessions import Request
+
 
 serviceKey="aEwwPiJLOfQLF991Fvvcv6piLOUwnO7uH7qiX6DXVazK8EDb6Yw39Hsq7xA+F4ux1KvKgLNVTx3CP7/IvdpOtQ=="
+service = unquote(serviceKey, 'UTF-8')
 
 def showBin():
     data = []
-    bean_kind =[]
-    url = "https://api.odcloud.kr/api/15087773/v1/uddi:6bcb9ebf-d368-4ac8-9382-a9e82437f74d?page=1&perPage=10&returnType=XML&serviceKey=aEwwPiJLOfQLF991Fvvcv6piLOUwnO7uH7qiX6DXVazK8EDb6Yw39Hsq7xA%2BF4ux1KvKgLNVTx3CP7%2FIvdpOtQ%3D%3D"
-    res = requests.get(url)
+    where =[]
+
+    url = "https://api.odcloud.kr/api/15087773/v1/uddi:6bcb9ebf-d368-4ac8-9382-a9e82437f74d"
+    returnType = "XML"
+    page = "1"
+    perPage = "10"
+
+    queryParams = '?' + urlencode({ quote_plus('serviceKey') : service, quote_plus('page') : page, quote_plus('perPage') : perPage, quote_plus('returnType') : returnType})
+    res = requests.get(url + queryParams)
+
+
+    #url = "https://api.odcloud.kr/api/15087773/v1/uddi:6bcb9ebf-d368-4ac8-9382-a9e82437f74d?page=1&perPage=10&returnType=XML&serviceKey=aEwwPiJLOfQLF991Fvvcv6piLOUwnO7uH7qiX6DXVazK8EDb6Yw39Hsq7xA%2BF4ux1KvKgLNVTx3CP7%2FIvdpOtQ%3D%3D"
+    #get = urllib.request.urlopen(url).read().decode('utf-8')
+    #res = requests.get(get)
     xml = res.text
     soup = BeautifulSoup(xml,'html.parser')
-    for tag in soup.find_all('소재지지번주소'):
-        data.append(tag.text)
-    for tag in soup.find_all('쓰레기통형태'):
-        bean_kind.append(tag.text)
-    res= dict(zip(data,bean_kind))
+    # for tag in soup.find_all('수거쓰레기종류'):
+    #     data.append(tag.text)
+    # for tag in soup.find_all('소재지도로명주소'):
+    # #     where.append(tag.text)
+    # for tag in soup.find_all():
+    #     data.append(tag.text)
+    data.append("kim")
+    where.append(soup.text)
+    res= dict(zip(data,where))
     return res
--- Behappy/FindLocation/views.py
+++ Behappy/FindLocation/views.py
@@ -2,21 +2,29 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from urllib.parse import urlencode, unquote
 from bs4 import BeautifulSoup
+from django.http.response import HttpResponse
 import requests
 import json
+
+from urllib3.response import HTTPResponse
 from .api import showBin
+import xml.etree.ElementTree as elemTree
 
 @csrf_exempt
 def showMap(request):
     if request.method =='GET':
         return render(request,'findlocation/map.html')
 
-serviceKey = "	fZrdoxTt5AoPpbAJScuxo3IeZBzRVqrhnG%2FpP7J6uZfC05FIbniTRaZicjkRyJr8Tzs0RdKmFnQgRFUNPUyXDA%3D%3D"
-serviceKeyDecoded = unquote
 
-@csrf_exempt
+
+
 def index(request):
     res = showBin()
-    bin = res.get('일반 사각 쓰레기통')
-    context = {'data': '서울특별시 구로구 구로동로 31', 'bin_kind' : bin}
-    return render(request,'findlocation/show.html',context)
+    print(res)
+    context = {'data' : '일반쓰레기','where' : res}
+    print(context)
+    return render(request, 'findlocation/show.html', context)
+    #return HTTPResponse(res)
+    # bin = res.get('일반 사각 쓰레기통')
+    # context = {'data': '서울특별시 구로구 구로동로 31', 'bin_kind' : bin}
+    # return render(request,'findlocation/show.html',context)

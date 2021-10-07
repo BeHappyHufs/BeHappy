b5d4c7cafe3b416de32b1d420737da4a30b8bfa7
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-10-08 00:31:03 +0900

    Fix : "부분 수정" 코드 수정 및 주석 처리

--- Behappy/FindLocation/api.py
+++ Behappy/FindLocation/api.py
@@ -12,7 +12,7 @@ serviceKey="aEwwPiJLOfQLF991Fvvcv6piLOUwnO7uH7qiX6DXVazK8EDb6Yw39Hsq7xA+F4ux1KvK
 service = unquote(serviceKey, 'UTF-8')
 
 
-def checkNum(num):
+def checkNum(num): # 숫자 여부를 판단하는 함수
     try:
         tmp = float(num)
         return True
@@ -24,34 +24,33 @@ def showBin():
     where =[]
     kind = []
 
-    for i in range(1,2):
-        url = "https://api.odcloud.kr/api/15087773/v1/uddi:6bcb9ebf-d368-4ac8-9382-a9e82437f74d"
-        returnType = "XML"
-        page = "1"
-        perPage = "280"
+ 
+    url = "https://api.odcloud.kr/api/15087773/v1/uddi:6bcb9ebf-d368-4ac8-9382-a9e82437f74d"
+    returnType = "XML"
+    page = "1"
+    perPage = "280"
 
-        queryParams = '?' + urlencode({ quote_plus('serviceKey') : service, quote_plus('page') : i, quote_plus('perPage') : perPage, quote_plus('returnType') : returnType})
-        res = requests.get(url + queryParams)
+    queryParams = '?' + urlencode({ quote_plus('serviceKey') : service, quote_plus('page') : page, quote_plus('perPage') : perPage, quote_plus('returnType') : returnType})
+    res = requests.get(url + queryParams)
 
     
-        xml = res.text
-        soup = BeautifulSoup(xml,'html.parser')
-        for tag in soup.find_all('col', attrs={'name':'위도'}):
-            check = str(tag.next)
-            print(check)
-            if checkNum(check):
-                data.append(tag.next)
-        for tag in soup.find_all('col', attrs={'name':'경도'}):
-            check = str(tag.next)
-            if checkNum(check):
-                where.append(tag.next)
-        for tag in soup.find_all('col', attrs={'name':'수거쓰레기종류'}):
-            kind.append(tag.next)
+    xml = res.text
+    soup = BeautifulSoup(xml,'html.parser')
+    for tag in soup.find_all('col', attrs={'name':'위도'}):
+        check = str(tag.next)
+        if checkNum(check):
+            data.append(tag.next)
+    for tag in soup.find_all('col', attrs={'name':'경도'}):
+        check = str(tag.next)
+        if checkNum(check):
+            where.append(tag.next)
+    for tag in soup.find_all('col', attrs={'name':'수거쓰레기종류'}):
+        kind.append(tag.next)
 
     result =[]
     result.append(data)
     result.append(where)
     result.append(kind)
-    print(result)
+    # print(result) # result 에 대한 내용을 출력
     return result
 
--- Behappy/FindLocation/urls.py
+++ Behappy/FindLocation/urls.py
@@ -1,5 +1,4 @@
 
-
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include

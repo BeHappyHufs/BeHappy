b3965dfcf6267df0001ced6ef70e8c6617059c42
lungnahahd <67555400+lungnahahd@users.noreply.github.com>
2021-11-15 17:26:12 +0900

    Feat : "Branch_Do 생성"

--- Behappy/Behappy/settings.py
+++ Behappy/Behappy/settings.py
@@ -92,7 +92,7 @@ DATABASES = {
 	    'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'NoticeBoard', 
         'USER': 'root', 
-        'PASSWORD': 'qwe123!@#', 
+        'PASSWORD': 'khd10810', 
         'HOST': 'localhost', 
         'PORT': '3306', 
     } 
